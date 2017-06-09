594dd5a53a857556793f00af495c4ec6534b922a
Andrei Bechet <andreibechet@Andreis-Work-MacBook-Pro.local>
2017-06-09 16:35:21 +0200

    monoids, and unboxing types with foldMap

--- create_types_with_semigroups.js
+++ create_types_with_semigroups.js
@@ -1,3 +1,5 @@
+const {Map, List} = require('immutable-ext')
+
 // const res = "a".concat("b").concat("c") // doesn't change types ... semigroups 
 // const res = [1,2].concat([3,4].concat([5,6]))
 // semigroups from maths => associativity 
@@ -29,10 +31,21 @@ const First = x =>
         First(x),
     inspect: () => `Sum(${x})`
 })
+// cannot be promoted to a monoid 
+
+const sum = xs => 
+    xs.reduce((acc, x) => acc + x, 0)
+
+const all = xs =>
+    xs.reduce((acc, x) => acc && x, true)
 
 
 // const res = Sum.empty().concat(Sum(1).concat(Sum(2)))
-const res = All.empty().concat(All(true).concat(All(true)))
+// const res = All.empty().concat(All(true).concat(All(true)))
 // const res = First("blah").concat(First("ice cream"))
+const res = Map({brian: 1, sara: 2, george: 3})
+            // .map(Sum)
+            // .fold(Sum.empty()) // removal from a type: collection relying on a monoid / single type
+            .foldMap(Sum, Sum.empty()) // map + fold 
 
 console.log(res)
